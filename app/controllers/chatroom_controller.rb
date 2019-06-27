371c6382fb24b168706c6965cc4483514d869c95
Rus Hughes <russell.hughes@gmail.com>
2019-06-27 22:03:56 +0200

    s8 l200

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -4,7 +4,8 @@ class ChatroomController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = Message.all
+    #@messages = Message.all
+    @messages = Message.custom_display
   end
 
 end
